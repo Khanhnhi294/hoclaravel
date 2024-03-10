fdbe8d5879b39bf61b9ad5dbab87db0bf0f22aab
KhanhNhi <nhi.tuong25@student.passerellesnumeriques.org>
2024-03-10 23:18:11 +0700

    Nhi/lession22

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
 use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
